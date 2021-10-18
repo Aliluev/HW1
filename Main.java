d88340cb78bec3334de25eb4c13b0a6b95443695
Aliluev Artem <Tema200133@gmail.com>
2021-10-18 12:51:19 +0300

    Circle

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.mycompany;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle example1=new Circle(2,"yellow");
+        System.out.println(example1.toString());
+        System.out.println(example1.getArea());
+
+    }
+}
